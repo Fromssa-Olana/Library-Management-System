a2d4a96f5ea9bd31d1e0ee0cae1649355810c941
Fromssa Olana <fromssa.olana@gmail.com>
2020-11-11 13:18:23 -0600

    added findAllThatContains method to help filter the data

--- src/main/java/com/librarymanagement/application/data/service/BookService.java
+++ src/main/java/com/librarymanagement/application/data/service/BookService.java
@@ -1,15 +1,13 @@
 package com.librarymanagement.application.data.service;
 
 import com.librarymanagement.application.data.Repository.BookRepository;
-import com.librarymanagement.application.data.Repository.MemberRepository;
-import com.librarymanagement.application.data.entity.AbstractEntity;
 import com.librarymanagement.application.data.entity.Book;
-import com.librarymanagement.application.data.entity.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class BookService {
@@ -19,21 +17,51 @@ public class BookService {
         this.bookRepository = bookRepository;
     }
 
-    public List<Book> findAll(){
+    public List<Book> findAllThatContains() {
         return bookRepository.findAll();
     }
 
-    public void update(Book book) {
-        findAll();
-    }
 
-
+    /**
+     *  It search a book object by its id
+     * @param id
+     * @return
+     */
     public Optional<Book> get(Integer id) {
         return bookRepository.findById(id);
     }
 
-
+    /**
+     *  It saves an object the is given
+     * @param book
+     *             void
+     */
     public void save(Book book) {
-            bookRepository.save(book);
+        bookRepository.save(book);
     }
+
+    /**
+     *  It filters the list according to the input given
+     *
+     * @param value
+     * @return
+     *          filtered list of books
+     */
+    public List<Book> findAllThatContains(String value) {
+
+        List<Book> filtered;
+        if (value == null || value.isEmpty()) {
+            return findAllThatContains();
+        } else {
+            filtered = findAllThatContains().stream()
+                    .filter(e -> e.getAuthor().toLowerCase().contains(value.toLowerCase())
+                            || e.getTitle().toLowerCase().contains(value.toLowerCase()))
+                    .collect(Collectors.toList());
+
+        }
+        return filtered;
+
+    }
+
+
 }
