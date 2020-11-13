c84419a1b37332e8b48b50ce42572421f0853f4b
Fromssa Olana <fromssa.olana@gmail.com>
2020-11-13 14:17:29 -0600

    added filter TextField, add book and add member button are added  accordingly

--- src/main/java/com/librarymanagement/application/backend/service/MemberService.java
+++ src/main/java/com/librarymanagement/application/backend/service/MemberService.java
@@ -1,12 +1,14 @@
 package com.librarymanagement.application.backend.service;
 
 import com.librarymanagement.application.backend.Repository.MemberRepository;
+import com.librarymanagement.application.backend.entity.Book;
 import com.librarymanagement.application.backend.entity.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MemberService {
@@ -32,5 +34,22 @@ public class MemberService {
 
     public void save(Member member) {
         memberRepository.save(member);
+    }
+
+
+    public List<Member> findAll(String value) {
+
+        List<Member> filtered;
+        if (value == null || value.isEmpty()) {
+            return findAll();
+        } else {
+            filtered = findAll().stream()
+                    .filter(e -> e.getFirstName().toLowerCase().contains(value.toLowerCase())
+                            || e.getLastName().toLowerCase().contains(value.toLowerCase()))
+                    .collect(Collectors.toList());
+
+        }
+        return filtered;
+
     }
 }
